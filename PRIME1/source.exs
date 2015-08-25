e51a737a11e333e5efee62ad031ddacdd59b0bea
Victor campos <victor.campos@visagio.com>
2015-08-25 12:04:32 -0300

    third solution to PRIME1, it's to slow

--- PRIME1/source.exs
+++ PRIME1/source.exs
@@ -5,26 +5,33 @@ defmodule PrimeGen do
 
   def find_primes(t, [a,b]) when (is_binary(a) or is_binary(b)), do:  find_primes(t, (String.to_integer(a)..String.to_integer(b)))
   def find_primes(t, numbers) do
+
     numbers |> Enum.reject(&rem(&1, 2) == 0)
-            |> Enum.filter(&is_prime(&1))
+            |> filter_primes([])
+            |> Enum.reverse
             |> Enum.each(&IO.puts(&1))
     IO.puts(' ')
     primes(t-1)
   end
 
-  defp is_prime(1), do: false
-  defp is_prime(a) do
-    is_prime(3,a)
+  defp filter_primes([], primes), do: primes
+  defp filter_primes([head|tail], primes), do: filter_primes([head|tail], primes, is_prime(head))
+  defp filter_primes([_|tail], primes, false), do: filter_primes(tail, primes)
+  defp filter_primes([head|tail], primes, true) do
+    primes = [head] ++ primes
+    tail |> Enum.reject(&rem(&1, head) == 0) |> filter_primes(primes)
   end
 
-  defp is_prime(divisor, number), do: is_prime(divisor, number, round(:math.sqrt(number) + 1))
-
+  defp is_prime(1), do: false
+  defp is_prime(a) do
+    is_prime(3,a, round(:math.sqrt(a) + 1))
+  end
   defp is_prime(divisor, _, stop) when divisor >= stop, do: true
-  defp is_prime(divisor, number, _) do
+  defp is_prime(divisor, number, stop) do
     if  rem(number, divisor) == 0 do
       false
     else
-      is_prime(divisor + 2, number)
+      is_prime(divisor + 2, number, stop)
     end
   end
 end
